55cfb5d65a8c935ba8ba15d10f09c175274a4d9a
Şerif <gungoronline@gmail.com>
2017-05-29 14:10:20 +0300

    Add files via upload

---
+++ PiCamera_Works/picamera_work_001.py
@@ -0,0 +1,17 @@
+﻿import picamera
+from time import sleep
+
+camera = picamera.PiCamera()
+
+camera.start_preview()
+camera.resolution = (480,640)
+camera.capture("a.jpg")
+sleep(0.02)
+camera.capture("b.jpg")
+sleep(0.02)
+camera.capture("c.jpg")
+sleep(0.02)
+camera.capture("d.jpg")
+sleep(0.02)
+camera.capture("e.jpg")
+camera.stop_preview()
